b96738ac8bb8273beeb3ff116dea506904f8cffc
RyanD177 <w0477770@campus.nscc.ca>
2024-03-26 09:09:18 -0300

    Better name for functions: Importing navigation for my header, and basic cssfont size, flex etc

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -3,30 +3,43 @@
   import { onMount } from "svelte";
   import {
     getSearchResults,
-    getToken,
+    getServerSideProps,
     searchResults,
-    authToken,
+    token,
   } from "../getToken.svelte";
+  import Navigation from "../../components/Navigation.svelte";
+
 
   const query = $page.url.searchParams.get("q");
   let filteredResults = [];
 
+
   onMount(async () => {
-    getToken().then(() => {
-      authToken.subscribe((value) => {
+    getServerSideProps().then(() => {
+      token.subscribe((value) => {
         getSearchResults(value, query);
       });
     });
-
     searchResults.subscribe((value) => {
       filteredResults = value;
+      console.log(filteredResults);
+      filteredResults.sort((a, b) => {
+        if (a.data.name.en_US < b.data.name.en_US) {
+          return -1;
+        }
+        if (a.data.name.en_US > b.data.name.en_US) {
+          return 1;
+        }
+        return 0;
+      });
     });
   });
 </script>
 
+<Navigation />
 {#each filteredResults as item}
   <a href="auctions/{item.data.id}">
-    <h1>{item.data.name.en_US}</h1>
+    <h1 style="color: red; text-decoration:none; ">{item.data.name.en_US}</h1>
 
     {#if item.data.media}
       <!-- svelte-ignore a11y-missing-attribute -->
